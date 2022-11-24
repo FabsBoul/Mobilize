734730c61f92996467076b615d7fa50f740eac0d
FabsBoul <fabsenlanne@gmail.com>
2022-11-24 01:48:44 +0100

    update exits

--- src/main.ts
+++ src/main.ts
@@ -11,39 +11,10 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.onEnterLayer('BackHomeZone').subscribe(()=>{
-        WA.nav.goToRoom("map.json");
-
-    })
 //    WA.room.onEnterLayer('CalcToRoom18').subscribe(()=>{
 //        WA.nav.goToRoom("map18.json");
 
  //   })
-
-    WA.room.onEnterLayer('CalcToRoom35').subscribe(()=>{
-        WA.nav.goToRoom("35/map35.json");
-
-    })
-
-    WA.room.onEnterLayer('CalcToRoomB2C').subscribe(()=>{
-        WA.nav.goToRoom("B2C/mapB2C.json");
-
-    })
-
-    WA.room.onEnterLayer('CalcToRoomB2G').subscribe(()=>{
-        WA.nav.goToRoom("B2G/mapB2G.json");
-
-    })
-
-    WA.room.onEnterLayer('CalcToRoomB2E').subscribe(()=>{
-        WA.nav.goToRoom("B2E/mapB2E.json");
-
-    })
-
-    WA.room.onEnterLayer('CalcToRoomCorp').subscribe(()=>{
-        WA.nav.goToRoom("Corp/mapCorp.json");
-
-    })
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
